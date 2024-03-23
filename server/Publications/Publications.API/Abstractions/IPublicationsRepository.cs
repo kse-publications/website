406fc9538004de9e214f97793c4d1e0dd2598180
Vlad Prudius <vladvlpr006@gmail.com>
2024-03-23 09:06:52 +0200

    feat(backend): add abstract IPublicationsRepository

---
+++ server/Publications/Publications.API/Abstractions/IPublicationsRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Publications.API.Abstractions;
+
+public interface IPublicationsRepository
+{
+    Task<IReadOnlyCollection<IPublication>> GetAllAsync(
+        CancellationToken cancellationToken = default);
+    
+    Task<IReadOnlyCollection<IPublication>> GetByFullTextSearchAsync(
+        string searchTerm, CancellationToken cancellationToken = default);
+    
+    Task<IReadOnlyCollection<IPublication>> GetByAutoCompleteAsync(
+        string searchTerm, CancellationToken cancellationToken = default);
+    
+    Task<IPublicationsDetails> GetByIdAsync(
+        Guid id, CancellationToken cancellationToken = default);
+    
+    Task InsertOrUpdateAsync(
+        IEnumerable<IPublicationsDetails> publications,
+        CancellationToken cancellationToken = default);
+}
