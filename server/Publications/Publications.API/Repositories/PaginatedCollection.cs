1eabd687d6ad6362a116b15b354120ccd51c924f
Vlad Prudius <vladvlpr006@gmail.com>
2024-03-26 12:34:26 +0200

    fix(backend): fixed PaginatedCollection serialization

--- server/Publications/Publications.API/Repositories/PaginatedCollection.cs
+++ server/Publications/Publications.API/Repositories/PaginatedCollection.cs
@@ -1,12 +1,4 @@
-﻿using System.Collections;
-using Publications.API.Models;
-
+﻿
 namespace Publications.API.Repositories;
 
-public record PaginatedCollection<T>(
-    IReadOnlyCollection<T> Items, int Count) : IEnumerable<T>
-{
-    public IEnumerator<T> GetEnumerator() => Items.GetEnumerator();
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-}
+public record PaginatedCollection<T>(IReadOnlyCollection<T> Items, int Count);
