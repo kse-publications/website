eddfc560ef2b485416188c13a1ca1576be2497bc
Tetiana Stasiuk <tanastasuk35@gmail.com>
2024-03-27 21:02:11 +0200

    bug fix

--- client/src/services/publicationsApiService.ts
+++ client/src/services/publicationsApiService.ts
@@ -2,12 +2,6 @@ import axios from 'axios'
 
 const BASE_URL = 'https://publications_api_testing.kse.ua/'
 
-axios.interceptors.request.use((config) => {
-  config.headers['Content-Type'] = 'application/json'
-  config.headers['Access-Control-Allow-Origin'] = '*'
-  return config
-})
-
 export default class PublicationsApiService {
   static getPublication(id: string) {
     return axios.get(BASE_URL + `publications/${id}`)
