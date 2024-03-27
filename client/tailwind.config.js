946f5d6503ea24400e942fadd2f0af51f45398b2
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2024-03-27 05:27:40 +0200

    feat(client): add search pages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   prefix: '',
   theme: {
+    maxWidth: {
+      main: '1160px',
+    },
     container: {
       center: true,
       padding: '2rem',
