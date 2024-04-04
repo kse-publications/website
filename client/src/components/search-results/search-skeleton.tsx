c882d18ad8a836f92904275b66ec3a2355ee06a9
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2024-04-04 16:21:21 +0300

    fix(client): fixed search skeleton

--- client/src/components/search-results/search-skeleton.tsx
+++ client/src/components/search-results/search-skeleton.tsx
@@ -7,8 +7,13 @@ export const SearchSkeleton = () => {
     <div className="mb-4 grid grid-cols-2 gap-4">
       {Array.from({ length: SKELETON_CARDS_COUNT }).map((_, index) => (
         <Card key={index} className="p-4">
-          <Skeleton className="mb-2 h-[56px] w-full" />
-          <Skeleton className="h-[16px] w-1/2" />
+          <div className="mb-4 flex gap-2">
+            <Skeleton className="h-6 w-14" />
+            <Skeleton className="h-6 w-40" />
+          </div>
+          <Skeleton className="mb-2 h-14 w-full" />
+          <Skeleton className="mb-1 h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/2" />
         </Card>
       ))}
     </div>
