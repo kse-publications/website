58a7064c2c48ae510c372381f2eabc4044c71c72
Vlad Prudius <vladvlpr006@gmail.com>
2024-04-14 07:39:55 +0300

    fix(backend): request analytics cookie -> header

--- server/Publications/Publications.API/Middleware/RequestAnalyticsFilterAttribute.cs
+++ server/Publications/Publications.API/Middleware/RequestAnalyticsFilterAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using Coravel.Queuing.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
 using Publications.API.BackgroundJobs;
 using Publications.API.Models;
 
@@ -24,21 +25,35 @@ public class RequestAnalyticsFilterAttribute : TypeFilterAttribute
         public async Task OnActionExecutionAsync(
             ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            if (context.HttpContext.Request.Cookies.TryGetValue("client-uuid", out string? sessionId))
+            if (ContainsClientUuid(context, out string? clientUuid))
             {
-                string resourceName = (context.HttpContext.Request.Path.Value?.Split(
-                        separator: '/', StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>())
-                    .First().ToLower(); 
+                string resourceName = (context.HttpContext.Request.Path.Value?
+                        .Split('/', StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>())
+                    .First()
+                    .ToLower(); 
             
                 // if the RequestAnalyticsFilter is used after the IdExtractionFilter in the filters pipeline
                 // then the numeric id will be already extracted from the slug
                 int resourceId = int.Parse(context.ActionArguments["id"]!.ToString()!);
 
-                Request request = new(sessionId, resourceName, resourceId);
+                Request request = new(clientUuid!, resourceName, resourceId);
                 _queue.QueueInvocableWithPayload<StoreRequestAnalyticsTask, Request>(request);
             }
             
             await next();
+        }
+        
+        private static bool ContainsClientUuid(ActionExecutingContext context, out string? clientUuid)
+        {
+            clientUuid = null;
+            
+            if (context.HttpContext.Request.Headers.TryGetValue("client-uuid", out StringValues values) &&
+                values.Count > 0)
+            {
+                clientUuid = values.First();
+            }
+            
+            return clientUuid is not null;
         }
     }
 }
