bca64656dfbb033738758a734e5a26ec3965ee73
Vlad Prudius <vladvlpr006@gmail.com>
2024-04-23 11:53:30 +0300

    feat(backend): add filters repository, service

---
+++ server/Publications/Publications.API/Repositories/Filters/IFiltersRepository.cs
@@ -0,0 +1,13 @@
+﻿using Publications.API.Models;
+
+namespace Publications.API.Repositories.Filters;
+
+public interface IFiltersRepository
+{
+    Task<IReadOnlyCollection<FilterGroup>> GetFiltersAsync(
+        CancellationToken cancellationToken = default);
+    
+    Task InsertOrUpdateAsync(
+        IEnumerable<FilterGroup> filters, 
+        CancellationToken cancellationToken = default);
+}
