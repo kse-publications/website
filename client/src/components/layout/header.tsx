f02a58eb02086c4b3185f7f6c22df1a69d87b27c
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2024-04-30 22:23:23 +0300

    fix-(client): fix animation & adaptation

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -5,36 +5,36 @@ import MobileMenuDrawer from './mobile-menu'
 
 function Header({ light = false }) {
   return (
-    <header
-      className={`w-100% mb-10 flex flex-row flex-wrap items-center justify-around px-4 py-7 ${light ? 'bg-transparent' : 'bg-[#31452B]'}`}
-    >
-      <a href="/">
-        <img width={160} height={40} src={logoLight.src} alt="KSE logo" />
-      </a>
+    <header className={`mb-6 py-7 lg:mb-10 ${light ? 'bg-transparent' : 'bg-[#31452B]'}`}>
+      <div className="mx-auto flex w-full max-w-[1160px] flex-row flex-wrap items-center justify-between px-4">
+        <a href="/">
+          <img width={160} height={40} src={logoLight.src} alt="KSE logo" />
+        </a>
 
-      <div className="flex items-center justify-center">
-        <div className="flex items-center justify-end sm:hidden">
-          <MobileMenuDrawer />
-        </div>
+        <div className="flex items-center justify-center">
+          <div className="flex items-center justify-end sm:hidden">
+            <MobileMenuDrawer />
+          </div>
 
-        <div
-          className={`hidden space-x-7 rounded-full border border-white text-[17px] text-white sm:block`}
-        >
-          <a href="/about" aria-label="Go to About page">
-            <Button variant="ghost" className="rounded-full">
-              About
-            </Button>
-          </a>
-          <a href="/submissions" aria-label="Go to Submissions page">
-            <Button variant="ghost" className="rounded-full">
-              Submissions
-            </Button>
-          </a>
-          <a href="/team" aria-label="Go to Team page">
-            <Button variant="ghost" className="rounded-full">
-              Team
-            </Button>
-          </a>
+          <div
+            className={`hidden space-x-5 rounded-full border border-white p-0.5 text-[17px] text-white sm:block`}
+          >
+            <a href="/about" aria-label="Go to About page">
+              <Button variant="ghost" className="rounded-full">
+                About
+              </Button>
+            </a>
+            <a href="/submissions" aria-label="Go to Submissions page">
+              <Button variant="ghost" className="rounded-full">
+                Submissions
+              </Button>
+            </a>
+            <a href="/team" aria-label="Go to Team page">
+              <Button variant="ghost" className="rounded-full">
+                Team
+              </Button>
+            </a>
+          </div>
         </div>
       </div>
     </header>
