b59387d896be5500e74c997ee70caa89791715eb
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2024-05-23 11:37:45 +0300

    client-(fix): remove console.log

--- client/src/services/posthog/posthog.ts
+++ client/src/services/posthog/posthog.ts
@@ -2,7 +2,7 @@ const ENVIRONMENT = import.meta.env.PUBLIC_ENVIRONMENT
 
 const captureEvent = (event: string, properties?: Record<string, any>) => {
   if (ENVIRONMENT !== 'production') return
-  console.log(event, properties)
+
   window.posthog.capture(event, properties)
 }
 
