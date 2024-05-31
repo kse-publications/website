00ff30dfbd2d973ec56e8bac12a63fd387ef989c
Vlad Prudius <vladvlpr006@gmail.com>
2024-05-31 09:52:04 +0300

    feat(backend): add OverallStats & RecentStats models

---
+++ server/Publications/Publications.Application/Statistics/OverallStats.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Publications.Application.Statistics;
+
+public class OverallStats
+{
+    public int TotalPublicationsCount { get; set; }
+    public long TotalSearchesCount { get; set; }
+}
