fe7ca4047cc4cc19ec1920bcd9b2d21a43503115
Vlad Prudius <vladvlpr006@gmail.com>
2024-06-03 15:54:21 +0300

    fix(client): fix adapter check condition

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react'
 import vercel from '@astrojs/vercel/serverless'
 import node from '@astrojs/node'
 
-const adapter = process.env.ASTRO_ADAPTER === 'vercel' ? vercel() : node({ mode: 'standalone' })
+const adapter = process.env.ASTRO_ADAPTER === 'node' ? node({ mode: 'standalone' }) : vercel()
 
 // https://astro.build/config
 export default defineConfig({
