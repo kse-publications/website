65437f9659f57dcaad6a2a9c0a4080e6506d334d
Tetiana Stasiuk <tanastasuk35@gmail.com>
2024-06-10 12:13:24 +0300

    button functionality update

--- client/src/components/static-pages/go-back-button.tsx
+++ client/src/components/static-pages/go-back-button.tsx
@@ -1,26 +1,35 @@
+import React, { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { ChevronLeftIcon } from '@radix-ui/react-icons'
 
 import MainPageButton from './to-main-button'
 
 function GoBackButton() {
-  const goBack = () => {
-    window.history.back()
+  const [buttonLabel, setButtonLabel] = useState('Go Back')
+
+  useEffect(() => {
+    if (window.history.length <= 1) {
+      setButtonLabel('Go to Main Page')
+    } else {
+      setButtonLabel('Go Back')
+    }
+  }, [])
+
+  const handleGoBack = () => {
+    if (window.history.length > 1) {
+      window.history.back()
+    } else {
+      window.location.href = '/'
+    }
   }
 
   return (
-    <>
-      {window.history.length > 2 ? (
-        <div className="-ml-4.5 mb-4 flex items-center text-base">
-          <Button variant="link" onClick={goBack}>
-            <ChevronLeftIcon className="h-5 w-5" />
-            Go Back
-          </Button>
-        </div>
-      ) : (
-        <MainPageButton />
-      )}
-    </>
+    <div className="-ml-4.5 mb-4 flex items-center text-base">
+      <Button variant="link" onClick={handleGoBack}>
+        <ChevronLeftIcon className="h-5 w-5" />
+        {buttonLabel}
+      </Button>
+    </div>
   )
 }
 
