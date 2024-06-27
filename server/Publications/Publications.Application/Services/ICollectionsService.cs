91d31d168d242bed6a5233968907a846ea7409fe
Vlad Prudius <vladvlpr006@gmail.com>
2024-06-27 20:31:27 +0300

    feat(backend): add collection obj to response on /collection/{id}

---
+++ server/Publications/Publications.Application/Services/ICollectionsService.cs
@@ -0,0 +1,15 @@
+﻿using Publications.Application.DTOs.Request;
+using Publications.Application.DTOs.Response;
+using Publications.Domain.Collections;
+
+namespace Publications.Application.Services;
+
+public interface ICollectionsService
+{
+    Task<IReadOnlyCollection<Collection>> GetAllAsync(
+        CancellationToken cancellationToken = default);
+    
+    Task<CollectionData?> GetByIdAsync(
+        int id, PaginationDTO paginationDTO, 
+        CancellationToken cancellationToken = default);
+}
