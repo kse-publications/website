3d21756779e02dae3e756e3c6252f500fa7dbda2
Vlad Prudius <vladvlpr006@gmail.com>
2024-12-15 12:15:56 +0200

    fix: use import instead of common js in tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
@@ -82,5 +84,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 }
