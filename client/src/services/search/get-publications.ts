7b44b1ba8b809c8f4c38170e029a7d624328c932
Vlad Prudius <vladvlpr006@gmail.com>
2025-01-27 09:39:32 +0200

    fix: display 404 on client instead of 500 (when api returns 404)

--- client/src/services/search/get-publications.ts
+++ client/src/services/search/get-publications.ts
@@ -48,13 +48,15 @@ export const searchPublications = async ({
 
 export const getPublication = async (id: string, clientUuid?: string): Promise<any> => {
   const BASE_URL = getBaseUrl()
-  return fetch(`${BASE_URL}/publications/${id}`, {
+  const response = await fetch(`${BASE_URL}/publications/${id}`, {
     headers: clientUuid
       ? {
           'Client-Uuid': clientUuid,
         }
       : {},
-  }).then((response) => response.json())
+  })
+  const data = await response.json()
+  return { status: response.status, data }
 }
 
 export const getRelatedByAuthors = async (
